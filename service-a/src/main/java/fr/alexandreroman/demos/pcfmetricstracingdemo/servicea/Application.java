10df51da7b1f4ee83725c4a928a75f6a9f70a13d
Alexandre Roman <aroman@pivotal.io>
2019-01-17 01:15:16 +0100

    Init project

---
+++ service-a/src/main/java/fr/alexandreroman/demos/pcfmetricstracingdemo/servicea/Application.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.demos.pcfmetricstracingdemo.servicea;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@FeignClient("pcf-metrics-tracing-demo-service-b")
+interface ServiceClient {
+    @GetMapping("/call")
+    String call();
+}
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+class IndexController {
+    private final ServiceClient client;
+
+    @GetMapping("/call")
+    public String index() {
+        log.info("Calling service B from service A");
+        return "Calling service B ---> " + client.call();
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().anyRequest().permitAll().and()
+                .httpBasic().disable().csrf().disable();
+    }
+}
---
+++ service-c/src/main/java/fr/alexandreroman/demos/pcfmetricstracingdemo/servicec/Application.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.demos.pcfmetricstracingdemo.servicec;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+@Slf4j
+class IndexController {
+    @GetMapping("/call")
+    public String index() {
+        log.info("This is the end");
+        return "You just reached the end of the Internet!";
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().anyRequest().permitAll().and()
+                .httpBasic().disable().csrf().disable();
+    }
+}
